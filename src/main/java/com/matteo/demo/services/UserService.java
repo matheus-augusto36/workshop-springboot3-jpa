3b91c69a71b9b1776ec39b1e7ba4fa3dd738f62f
Matheus Augusto <silvamatheusaugusto36@gmail.com>
2022-10-14 18:20:54 -0300

    Exception handling - findById

--- src/main/java/com/matteo/demo/services/UserService.java
+++ src/main/java/com/matteo/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.matteo.demo.entities.User;
 import com.matteo.demo.repositories.UserRepository;
+import com.matteo.demo.services.exceptions.ResourceNotFoundException;
 
 @Service //Registra a classe no Spring para injeção de dependência
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
